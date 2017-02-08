d458e9bc7f60b52b5c3cab0f0e10098e5bd81985
Ryan Spangler <ryan@getlittlebird.com>
2017-02-07 18:35:00 -0800

    working on ophion js client package

--- client/js/src/ophion/ophion.js
+++ client/js/src/ophion/ophion.js
@@ -1,97 +1,105 @@
-
-
 function OphionQuery(parent) {
   var parent = parent;
   var query = [];
   
   return {
     label: function(label) {
-       query.push({'label': label});
-       return this;
+      query.push({'label': label});
+      return this;
     },    
     has: function(prop, within) {
-       query.push({'has': prop, 'within': within});
-       return this;
+      query.push({'has': prop, 'within': within});
+      return this;
     },
     values: function(v) {
-       query.push({'values': v})
-       return this
+      query.push({'values': v})
+      return this
     },
     incoming: function(label) {
-       query.push({'in': label})
-       return this
+      query.push({'in': label})
+      return this
     },
     outgoing: function(label) {
-       query.push({'out': label})
-       return this
+      query.push({'out': label})
+      return this
     },
     inEdge: function(label) {
-       query.push({'inEdge': label})
-       return this
+      query.push({'inEdge': label})
+      return this
     },
     outEdge: function(label) {
-       query.push({'outEdge': label})
-       return this
+      query.push({'outEdge': label})
+      return this
     },
     inVertex: function(label) {
-       query.push({'inVertex': label})
-       return this
+      query.push({'inVertex': label})
+      return this
     },
     outVertex: function(label) {
-       query.push({'outVertex': label})
-       return this
+      query.push({'outVertex': label})
+      return this
     },
     mark: function(label) {
-       query.push({'as': label})
-       return this
+      query.push({'as': label})
+      return this
     },
     limit: function(l) {
-       query.push({'limit': l})
-       return this
+      query.push({'limit': l})
+      return this
     },
     range: function(begin, end) {
-       query.push({'begin': begin, 'end': end})
-       return this
+      query.push({'begin': begin, 'end': end})
+      return this
     },
     count: function() {
-       query.push({'count': ''})
-       return this
+      query.push({'count': ''})
+      return this
     },
     groupCount: function(label) {
-        query.push({'groupCount': label})
-        return this
+      query.push({'groupCount': label})
+      return this
     },
     by: function(label) {
-        query.push({'by': label})
-        return this
+      query.push({'by': label})
+      return this
     },
     cap: function(c) {
-        query.push({'cap': c})
-        return this
+      query.push({'cap': c})
+      return this
     },
     execute: function(callback) {
-        parent.execute({query:query}, callback)
+      parent.execute({query:query}, callback)
     }
   };  
 }
 
 function Ophion() {
-  
-  var out = {
-      execute: function(query, callback) {
-        $.ajax({
-          url: "/gaia/vertex/query",
-          dataType: 'json',
-          type: 'POST',
-          data: JSON.stringify(query),
-          success: function(results) {
-            callback(results)
-          }
-        })        
-      },
-      query: function() {
-        return OphionQuery(this)
-      }
+  var queryBase = '/gaia/vertex/query';
+  return {
+    execute: function(query, callback) {
+      var request = fetch(queryBase, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(query),
+      })        
+
+      // $.ajax({
+      //   url: "/gaia/vertex/query",
+      //   dataType: 'json',
+      //   type: 'POST',
+      //   data: JSON.stringify(query),
+      //   success: function(results) {
+      //     callback(results)
+      //   }
+      // })        
+
+      request.when(function(data) {
+        callback(data)
+      });
+    },
+
+    query: function() {
+      return OphionQuery(this)
+    }
   }  
-  return out  
 }
--- client/js/webpack.config.js
+++ client/js/webpack.config.js
@@ -4,12 +4,11 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'src/ophion');
 
-var config = {
+module.exports = {
   entry: APP_DIR + '/ophion.js',
   output: {
+    library: 'ophion',
     path: BUILD_DIR,
     filename: 'ophion.js'
   }
 };
-
-module.exports = config;

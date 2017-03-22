acafc595a836a2b175ba6401bc9ecbed9a0c262d
Kyle Ellrott <kellrott@gmail.com>
2017-03-22 00:02:17 -0700

    Updating imports

--- build_goclient.sh
+++ build_goclient.sh
@@ -3,7 +3,7 @@
 export GOPATH=`pwd`
 
 go get github.com/golang/protobuf/protoc-gen-go/
-go get github.com/grpc-ecosystem/grpc-gateway/
+go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 export PATH=`pwd`/bin:$PATH
 pushd schema
 protoc -I./ -I../src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=Mgoogle/protobuf/struct.proto=github.com/golang/protobuf/ptypes/struct,\

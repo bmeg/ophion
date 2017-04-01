1b59124d17bafd65b35e7f8ef27cc79f25f5246c
Kyle Ellrott <kellrott@gmail.com>
2017-03-31 17:43:12 -0700

    Adding more methods to GO query builder

--- client/go/util.go
+++ client/go/util.go
@@ -2,8 +2,12 @@
 package ophion
 
 import (
+	"log"
+	"fmt"
 	"context"
 	"google.golang.org/grpc"
+	"github.com/golang/protobuf/jsonpb"
+	"encoding/json"
 )
 
 
@@ -29,9 +33,41 @@ func (q QueryBuilder) V(id ...string) QueryBuilder {
 	if len(id) > 0 {
 		return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_V{id[0]}}) }
 	} else {
-		return QueryBuilder{ q.client, append(q.query, &GraphStatement{}) }
+		return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_V{}}) }
 	}
 }
+
+func (q QueryBuilder) AddV(id string) QueryBuilder {
+	return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_AddV{id}}) }
+}
+
+func (q QueryBuilder) AddE(edgeType string) QueryBuilder {
+	return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_AddE{edgeType}}) }
+}
+
+
+func (q QueryBuilder) To(id string) QueryBuilder {
+	return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_To{id}}) }
+}
+
+func (q QueryBuilder) Property(v ...interface{}) QueryBuilder {
+	if len(v) == 1 {
+		return QueryBuilder{ q.client, append(q.query, &GraphStatement{
+			&GraphStatement_Property{ AsStruct(v[0].(map[string]interface{})) },
+		})}
+	} else {
+		return QueryBuilder{ q.client, append(q.query, &GraphStatement{
+			&GraphStatement_Property{ AsStruct( map[string]interface{}{
+				v[0].(string) : v[1],
+			} ) },
+		})}
+	}
+}
+
+func (q QueryBuilder) Count() QueryBuilder {
+	return QueryBuilder{ q.client, append(q.query, &GraphStatement{&GraphStatement_Count{}}) }
+}
+
 
 func (q QueryBuilder) Execute() (chan *ResultRow, error) {
 	tclient, err := q.client.Traversal(context.TODO(), &GraphQuery{q.query})
@@ -42,8 +78,27 @@ func (q QueryBuilder) Execute() (chan *ResultRow, error) {
 	go func() {
 		defer close(out)
 		for t, err := tclient.Recv(); err != nil; t, err = tclient.Recv() {
+			log.Printf("vert: %s\n", t)
 			out <- t
 		}
 	}()
 	return out, nil
+}
+
+func (q QueryBuilder) Run() error {
+	c, err := q.Execute()
+	if err != nil {
+		return err
+	}
+	for range c {}
+	return nil
+}
+
+func (q QueryBuilder) Render() map[string]interface{} {
+	m := jsonpb.Marshaler{}
+	s, _ := m.MarshalToString(&GraphQuery{q.query})
+	fmt.Printf("%s = %s\n", q, s)
+	out := map[string]interface{}{}
+	json.Unmarshal([]byte(s), &out)
+	return out
 }

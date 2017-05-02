b55554b168eec5398b509a35c5706d5f49239ffd
Ryan Spangler <ryan.spangler@gmail.com>
2017-05-01 23:43:45 -0700

    including index

--- src/ophion/elastic.clj
+++ src/ophion/elastic.clj
@@ -1,10 +1,11 @@
 (ns ophion.elastic
   (:require
-   [clojurewerkz.elastisch.rest :as esr]))
+   [clojurewerkz.elastisch.rest :as elastic]
+   [clojurewerkz.elastisch.rest.index :as index]))
 
 (defn connect
   [{:keys [host port]}]
-  (esr/connect
+  (elastic/connect
    (str
     "http://" (or host "127.0.0.1")
     ":" (or port "9200"))))

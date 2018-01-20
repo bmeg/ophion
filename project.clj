535802ad617c59a8a3a822d86df930a48fe5f223
Ryan Spangler <ryan.spangler@gmail.com>
2018-01-19 17:56:45 -0800

    protograph version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ophion "0.0.10"
+(defproject ophion "0.0.11"
   :description "graph queries as data"
   :url "http://github.com/bmeg/ophion"
   :license {:name "Eclipse Public License"
@@ -10,7 +10,7 @@
                  [com.taoensso/timbre "4.8.0"]
                  ;; [org.immutant/web "2.1.7"]
                  [http-kit "2.2.0"]
-                 [protograph "0.0.17"]
+                 [protograph "0.0.18"]
                  [clojurewerkz/elastisch "2.2.1" :exclusions [commons-codec]]
                  [com.novemberain/monger "3.1.0"]
                  [;;org.janusgraph/janusgraph-core "0.2.0-20170924.171957-19"

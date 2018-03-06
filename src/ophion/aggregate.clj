b623e0c80bc0181aac22f6c66978b727a10f63d4
Ryan Spangler <ryan.spangler@gmail.com>
2018-03-06 14:00:09 -0800

    unique

--- src/ophion/aggregate.clj
+++ src/ophion/aggregate.clj
@@ -99,7 +99,7 @@
     (to-vertex vertex-label where))))
 
 (defn from-unique
-  ([edge-label vertex-label] (from-dedup edge-label vertex-label {}))
+  ([edge-label vertex-label] (from-unique edge-label vertex-label {}))
   ([edge-label vertex-label where]
    (concat
     (from-edge edge-label)
@@ -107,7 +107,7 @@
     (from-vertex vertex-label where))))
 
 (defn to-unique
-  ([edge-label vertex-label] (to-dedup edge-label vertex-label {}))
+  ([edge-label vertex-label] (to-unique edge-label vertex-label {}))
   ([edge-label vertex-label where]
    (concat
     (to-edge edge-label)
